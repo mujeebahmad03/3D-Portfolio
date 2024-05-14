ee15efb2fc414eec52d82dc6c595a79c42e45dcb
Abdulmujeeb Ahmad <abdulmujeebahmad03@gmail.com>
2024-05-14 22:23:56 +0100

    3d portfolio

---
+++ src/assets/index.js
@@ -0,0 +1,67 @@
+import backend from "./backend.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import mobile_app from "./mobile_app.png"
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import next from "./tech/nextjs.svg"
+import python from "./tech/python.png"
+import django from "./tech/django.png"
+import postgres from "./tech/postgres.png"
+
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import web_img from "./web_img.png";
+import django_backend from "./django_backend.png";
+
+export {
+  backend,
+  mobile,
+  web,
+  web_img,
+  github,
+  django_backend,
+  menu,
+  close,
+  mobile_app,
+  css,
+  docker,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  next,
+  python,
+  django,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+  postgres
+};
---
+++ src/components/ServiceCard.jsx
@@ -0,0 +1,41 @@
+import { motion } from "framer-motion";
+import PropTypes from "prop-types";
+import Tilt from "react-parallax-tilt";
+
+import { fadeIn } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className="xs:w-[250px] w-full">
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+      >
+        <img
+          src={icon}
+          alt={title}
+          className="w-18 h-18 object-contain"
+        />
+
+        <h3 className="text-white text-[20px] font-bold text-center">
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+ServiceCard.propTypes = {
+  index: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+};
+
+export default ServiceCard;
